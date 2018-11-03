a730caf3f4d77c7f561ae2f3353374bf0369bfe2
joerg kost <jk@ip-clear.de>
2018-11-03 13:50:46 +0100

    - Version 0.5 tag

--- mlxsh.go
+++ mlxsh.go
@@ -72,7 +72,7 @@ func init() {
 	flag.Parse()
 
 	if version {
-		log.Println("mlxsh 0.4 (C) 2018 by Jörg Kost, jk@ip-clear.de")
+		log.Println("mlxsh 0.5 (C) 2018 by Jörg Kost, jk@ip-clear.de")
 		os.Exit(0)
 	}
 
